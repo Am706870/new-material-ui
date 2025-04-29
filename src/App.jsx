3bb9acfd726e31a6d77a116d7df0d0e3db980777
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-04-29 21:40:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Typography } from '@mui/material'
-import Header from './components/header'
+import Header from './components/Header'
 import CourseCard from './components/CourseCard'
 import './App.css'
 
